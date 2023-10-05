31646ea8403e8b36647063e70f307727c35b3d17
oayaka <131725511+oayaka@users.noreply.github.com>
2023-10-05 14:08:29 +0900

    Add files via upload

---
+++ omikuji/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+{
+    const btn = document.getElementById('btn');
+    const result = document.getElementById('result');
+
+    btn.addEventListener('click',() =>{
+        const results =['大吉','中吉','凶','小吉'];
+        const n =Math.floor(Math.random() * results.length);
+
+        result.textContent=results[n];
+
+        // switch(n){
+        //     case 0:
+        //         result.textContent = '大吉';
+        //         break;
+        //     case 1:
+        //         result.textContent = '中吉';
+        //         break;
+        //     case 2:
+        //         result.textContent = '凶';
+        //         break;
+        // }
+    })
+}
