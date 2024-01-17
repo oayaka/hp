0908f94880339c2a31f9f1777d05b6a5b62536d7
oayaka <131725511+oayaka@users.noreply.github.com>
2024-01-17 21:18:23 +0900

    20240117

---
+++ 20240117_後出しジャンケン/main.js
@@ -0,0 +1,163 @@
+console.log("main.js!!");
+
+/*
+// jQuery入門
+//	公式サイト: https://jquery.com/
+//	チュートリアル: https://snome.jp/programming/jquery1/
+//	チートシート: https://web-cheatsheet.com/jquery-to-vanillajs
+*/
+
+// Ready
+$(document).ready(()=>{
+	console.log("Ready!!");
+});
+
+let gameActive = false;
+let correctHand;
+let instruction; //命令
+let score = 0;
+
+let timer;
+
+
+function startGame() {
+	gameActive = true;
+	nextRound();
+	score = 0;
+}
+
+function nextRound() {
+	if (gameActive) {
+		correctHand = getRandomHand();
+		instruction = getRandomInstruction();
+		displayInstruction();
+		startTimer();
+	}
+}
+
+function getRandomHand() {
+	const hands = ['グー', 'チョキ', 'パー'];
+	const randomIndex = Math.floor(Math.random() * hands.length);
+	return hands[randomIndex];
+	
+	updateImage(selectedHand);
+
+
+}
+
+function getRandomInstruction() {
+    const instructions = ['勝って', '負けて', 'アイコ'];
+    const randomIndex = Math.floor(Math.random() * instructions.length);
+    return instructions[randomIndex];
+}
+
+function displayInstruction() {
+    const resultElement = document.getElementById('result');
+    const opponentImageElement = document.getElementById('my_img');
+    resultElement.textContent = `相手の手: ${correctHand}。${instruction}。`;
+    resultElement.style.color = 'black';
+
+    switch (correctHand) {
+        case 'グー':
+            opponentImageElement.src = "./images/guu.png";// グーの画像のパスに変更
+            break;
+        case 'チョキ':
+            opponentImageElement.src = "./images/choki.png"; // チョキの画像のパスに変更
+            break;
+        case 'パー':
+            opponentImageElement.src = "./images/paa.png"; // パーの画像のパスに変更
+            break;
+    }
+}
+
+
+
+function startTimer() {
+	timer = setTimeout(() => {
+		endGame();
+	}, 5000);
+}
+
+function endGame() {
+	gameActive = false;
+	displayResult('ゲームオーバー！');
+	
+}
+
+function checkHand(selectedHand) {
+	if (gameActive) {
+		clearTimeout(timer);
+		const winHands = {
+			'グー': 'パー',
+			'チョキ': 'グー',
+			'パー': 'チョキ'
+		};
+		const loseHands = {
+			'グー': 'チョキ',
+			'チョキ': 'パー',
+			'パー': 'グー'
+		};
+
+		switch (instruction) {
+			case '勝って':
+				if (winHands[correctHand] === selectedHand) {
+					score++;
+					displayResult('正解！');
+					
+				} else {
+					endGame();
+				}
+				break;
+			case '負けて':
+				if (loseHands[correctHand] === selectedHand) {
+					score++;
+					displayResult('正解！');
+					
+				} else {
+					endGame();
+				}
+				break;
+			case 'アイコ':
+				if (selectedHand === correctHand) {
+					score++;
+					displayResult('正解！');
+					
+				} else {
+					endGame();
+				}
+				break;
+		}
+
+		displayScore();
+		nextRound();
+	}
+
+	
+
+
+}
+
+function displayResult(message) {
+	const resultElement = document.getElementById('result');
+	resultElement.textContent = message;
+	resultElement.style.color = message === '正解！' ? 'green' : 'red';
+}
+
+function displayScore() {
+	const scoreElement = document.getElementById('score');
+	scoreElement.textContent = `得点: ${score}`;
+}
+
+
+
+
+// アニメーション
+function doAnimation(id, type){
+	const elem = $(id);
+	elem.addClass("animate__animated");
+	elem.addClass(type);
+	elem.on("animationend", ()=>{
+		elem.off("animationend");
+		elem.removeAttr("class");
+	});
+}
